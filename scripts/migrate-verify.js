6b0c97fa445019e7c3faa42d64a71759bf14bed8
Ivan Poon <ravenxce@gmail.com>
2021-09-02 14:35:22 +0800

    Add piecewise distributor migration to avoid gas limit issues

---
+++ scripts/migrate-verify.js
@@ -0,0 +1,37 @@
+const { zilliqa } = require('./zilliqa')
+
+async function verify(legacyAddress, newAddress, maxEpoch) {
+  const oldContract = zilliqa.contracts.at(legacyAddress)
+  const newContract = zilliqa.contracts.at(newAddress)
+
+  const oldState = await oldContract.getState()
+  const newState = await newContract.getState()
+
+  for (let i = 0; i <= maxEpoch; ++i) {
+    console.log(`checking epoch ${i}`)
+    const epoch = i.toString()
+    const newMerkleRoot = newState.merkle_roots[epoch]
+    const oldMerkleRoot = oldState.merkle_roots[epoch]
+    if (newMerkleRoot !== oldMerkleRoot) {
+      throw new Error(`new state merkle root at epoch ${i} is ${newMerkleRoot}, expected ${oldMerkleRoot}`)
+    }
+    const newClaimedLeafs = Object.keys(newState.claimed_leafs[epoch])
+    const oldClaimedLeafs = Object.keys(oldState.claimed_leafs[epoch])
+    if (newClaimedLeafs.length === 0 || JSON.stringify(newClaimedLeafs) !== JSON.stringify(oldClaimedLeafs)) {
+      throw new Error(`new state claimed leafs at epoch ${i} is ${newClaimedLeafs}, expected ${oldClaimedLeafs}`)
+    }
+    console.log(`found ${newClaimedLeafs.length} claimed leafs`)
+  }
+
+  if (!!newState.merkle_roots[maxEpoch + 1] || !!newState.claimed_leafs[maxEpoch + 1]) {
+    throw new Error('new state has too many epochs')
+  }
+
+  console.log('Verification complete, state match success.')
+}
+
+verify(
+  process.env.LEGACY_CONTRACT,
+  process.env.NEW_CONTRACT,
+  parseInt(process.env.MAX_EPOCH, 10)
+)
--- scripts/migrate.js
+++ scripts/migrate.js
@@ -7,70 +7,77 @@
 const { BN, Long } = require('@zilliqa-js/util');
 const { VERSION, zilliqa, useKey } = require('./zilliqa')
 
-async function test(privateKey, legacyAddress, newAddress) {
+async function migrate(privateKey, legacyAddress, newAddress, minEpoch, maxEpoch) {
   useKey(privateKey)
   const minGasPrice = (await zilliqa.blockchain.getMinimumGasPrice()).result
-  const legacyContract = zilliqa.contracts.at(legacyAddress)
-  const callTx = await legacyContract.call(
-    'SetMerkleRoot',
-    [
-      {
-        vname: 'epoch_number',
-        type: 'Uint32',
-        value: '0',
-      },
-      {
-        vname: 'merkle_root',
-        type: 'ByStr32',
-        value: '0x0123456789012345678901234567890123456789012345678901234567890123'
-      },
-    ],
-    {
-      version: VERSION,
-      amount: new BN(0),
-      gasPrice: new BN(minGasPrice),
-      gasLimit: Long.fromNumber(25000),
-    },
-    33,
-    1000,
-    false,
-  )
-  if (!callTx.receipt || !callTx.receipt.success) {
-    console.log(JSON.stringify(callTx.receipt, null, 4))
-    throw new Error('Failed to set merkle root')
+  const newContract = zilliqa.contracts.at(newAddress)
+  const balance = await zilliqa.blockchain.getBalance(zilliqa.wallet.defaultAccount.address);
+  console.log({ nonce: balance.result.nonce, minGasPrice })
+  const nonce = balance.result.nonce + 1
+
+  const promises = []
+  for (let epoch = minEpoch; epoch <= maxEpoch; ++epoch) {
+    promises.push(new Promise(async (resolve) => {
+      console.log(`Sending tx for epoch ${epoch}, nonce: ${nonce + epoch - minEpoch}`)
+      const tx = await newContract.call(
+        'MigrateData',
+        [
+          {
+            vname: 'legacy_contract',
+            type: 'ByStr20',
+            value: legacyAddress
+          },
+        ],
+        {
+          version: VERSION,
+          amount: new BN(0),
+          gasPrice: new BN(minGasPrice),
+          gasLimit: Long.fromNumber(33000),
+          nonce: nonce + epoch - minEpoch,
+        },
+        33,
+        1000,
+        false,
+      )
+      resolve(tx)
+    }))
+  }
+  const results = await Promise.all(promises).catch(err => console.error(err))
+  console.log({results})
+
+  for (let i = 0; i < results.length; ++i) {
+    const migrateTx = results[i]
+    if (!migrateTx.receipt || !migrateTx.receipt.success) {
+      console.log(JSON.stringify(migrateTx.receipt, null, 4))
+      throw new Error(`Failed to migrate data for epoch ${minEpoch + i}`)
+    }
   }
 
-  const newContract = zilliqa.contracts.at(newAddress)
-  const migrateTx = await newContract.call(
-    'MigrateData',
-    [
-      {
-        vname: 'legacy_contract',
-        type: 'ByStr20',
-        value: legacyAddress
-      },
-    ],
+  const revokeTx = newContract.call(
+    'RevokeOwnership', [],
     {
       version: VERSION,
       amount: new BN(0),
       gasPrice: new BN(minGasPrice),
-      gasLimit: Long.fromNumber(25000),
+      gasLimit: Long.fromNumber(10000),
     },
     33,
     1000,
     false,
   )
-  if (!migrateTx.receipt || !migrateTx.receipt.success) {
-    console.log(JSON.stringify(migrateTx.receipt, null, 4))
-    throw new Error('Failed to migrate data')
+  if (!revokeTx.receipt || !revokeTx.receipt.success) {
+    console.log(JSON.stringify(revokeTx.receipt, null, 4))
+    throw new Error('Failed to revoke ownership')
   }
 
   const state = await newContract.getState()
   console.log(`New contract state:\n${JSON.stringify(state, null, 2)}`)
 }
 
-test(
+migrate(
   process.env.PRIVATE_KEY,
   process.env.LEGACY_CONTRACT,
   process.env.NEW_CONTRACT,
+  parseInt(process.env.MIN_EPOCH || 0, 10),
+  parseInt(process.env.MAX_EPOCH, 10)
 )
